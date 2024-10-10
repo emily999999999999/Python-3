0edab947430c83763af62a9f1274d95dbfd80081
emily999999999999 <emilyveras56@gmail.com>
2024-10-10 11:08:37 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# Pedindo dados ao usuário
+nome = input("Digite o seu nome completo por favor ")
+idade = input("Qual é sua idade? ")
+
+# Exibindo os dados coletados
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+
+# Mensagem de agradecimento
+input("Obrigada pelas informações")
